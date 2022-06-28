a0742a09b171fbaa5669a588754886cb794496d5
rodrigoge <rgestevao@gmail.com>
2022-06-28 16:14:17 -0300

    creating Subscriber page

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4x6isow06ts01up3nwngjg5/master',
+    uri: import.meta.env.VITE_API_URI,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
